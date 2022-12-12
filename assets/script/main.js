4abafceb9531555c1c3c408675c03d1193ce8366
anoukbruinn <112856687+anoukbruinn@users.noreply.github.com>
2022-12-12 16:17:44 +0100

    done

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,23 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+
+let userflow = document.querySelector('a:nth-of-type(13)')
+
+userflow.addEventListener('click', shakeHandler)
+userflow.addEventListener('animationend', shakeHandler)
+
+function shakeHandler() {
+  userflow.classList.toggle('shake')
+}
+
+let frontend = document.querySelector('a:nth-of-type(1)')
+
+frontend.addEventListener('click', flipHandler)
+frontend.addEventListener('animationend', flipHandler)
+
+function flipHandler() {
+  frontend.classList.toggle('flip')
+}
+
