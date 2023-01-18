510ee122643d5d25453a59975e8178f6eb0eaa86
anoukbruinn <112856687+anoukbruinn@users.noreply.github.com>
2023-01-18 11:04:05 +0100

    Update main.js

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,4 +1,6 @@
-let interaction = document.querySelector('a:nth-of-type(12)')
+let interaction = document.querySelector('a:nth-of-type(2)')
+
+// console.log(document.querySelector('a:nth-of-type(4)'))
 
 interaction.addEventListener('click', jumpHandler)
 interaction.addEventListener('animationend', jumpHandler)
@@ -16,6 +18,9 @@ userflow.addEventListener('animationend', shakeHandler)
 function shakeHandler() {
   userflow.classList.toggle('shake')
 }
+
+const anouk = 'Anouk';
+console.log(anouk)
 
 let frontend = document.querySelector('a:nth-of-type(1)')
 
