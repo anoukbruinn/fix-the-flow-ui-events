8a9d930a3290aed838e8baa2def00dae8023552c
anoukbruinn <112856687+anoukbruinn@users.noreply.github.com>
2023-01-29 14:33:38 +0100

    done
    done

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,13 +1,14 @@
+/* 1e */
 let frontend = document.querySelector('a:nth-of-type(1)')
 
-frontend.addEventListener('click', flipHandler)
-frontend.addEventListener('animationend', flipHandler)
+frontend.addEventListener('click', frontendHandler)
+frontend.addEventListener('animationend', frontendHandler)
 
-function flipHandler() {
-  frontend.classList.toggle('flip')
+function frontendHandler() {
+  frontend.classList.toggle('frontend')
 }
 
-
+/* 2e */
 let interaction = document.querySelector('a:nth-of-type(2)')
 
 interaction.addEventListener('click', jumpHandler)
@@ -17,6 +18,7 @@ function jumpHandler() {
   interaction.classList.toggle('jump')
 }
 
+/* 3e */
 let and = document.querySelector("a:nth-of-type(3)");
 and.addEventListener("click", hangHandler);
 and.addEventListener("animationend", hangHandler);
@@ -24,8 +26,8 @@ function hangHandler() {
   and.classList.toggle("hang");
 }
 
-let document = document.querySelector('a:nth-of-type(4)')
-
+/* 4e */
+let development = document.querySelector('a:nth-of-type(4)')
 frontend.addEventListener('click', flipHandler)
 frontend.addEventListener('animationend', flipHandler)
 
